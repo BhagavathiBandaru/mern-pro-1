3680de4a73df6168401b556c4593d262cbc6063d
Bhagavathi <bhagavathibandaru0@gmail.com>
2023-05-11 12:11:58 +0530

    initial-commit

---
+++ routes/authRoute.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+// Forgot password
+router.post("/forgot-password", forgotPasswordController);
+
+// protected  USER route auth
+
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+// protected Admin route auth
+
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+export default router;
